d64df1e735766828d9707c21e141cb405e29f2e0
hluffy <18801613171@163.com>
2017-12-01 17:54:35 +0800

    upload

---
+++ src/main/java/com/welleplus/test/DeleteFile.java
@@ -0,0 +1,39 @@
+package com.welleplus.test;
+
+import java.io.File;
+
+public class DeleteFile {
+	public static void main(String[] args) {
+		String str1 = "D:/KuGou";
+		File file = new File(str1);
+		while(file.listFiles().length!=0){
+			getfiles(file);
+		}
+		
+	}
+	
+	public static void getfiles(File file){
+		if(file.isDirectory()){
+			File[] files = file.listFiles();
+			if(files.length==0){
+				file.delete();
+				System.out.println(file.getName());
+			}else{
+				for (File file1 : files) {
+					if(file1.isDirectory()){
+						getfiles(file1);
+					}else{
+						file1.delete();
+						System.out.println(file1.getName());
+					}
+					
+				}
+			}
+			
+		}else{
+			file.delete();
+			System.out.println(file.getName());
+		}
+	}
+
+}
